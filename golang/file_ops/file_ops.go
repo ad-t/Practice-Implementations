6774008acd40937104a7cb5aa6f84e978d52731c
Adam T <adam.tizzone@gmail.com>
2016-11-10 14:23:26 +1100

    achieved file directory listing

--- golang/file_ops/file_ops.go
+++ golang/file_ops/file_ops.go
@@ -2,9 +2,19 @@ package main
 
 import (
     "fmt"
-    "oi/ioutil"
+    "io/ioutil"
 )
 
 func main () {
-    fmt.Println(ioutil.ReadDir("./"))
+    dir, err := ioutil.ReadDir(".")
+    if err != nil {
+        fmt.Println("errors!")
+    }
+    for index, file := range dir {
+        if file.Size() > 50 {
+            fmt.Println("Big files! (", index, ")")
+        } else {
+            fmt.Println("small files :(")
+        }
+    }
 }
